b0a527bd49b3cfb1b7cf3513a3b238b892b7ee6d
Abagail Hudson <abagailanne@gmail.com>
2019-04-28 23:32:38 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -10,46 +10,6 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 
- def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
- def move(board, location, current_player = "X")
-  board[location.to_i-1] = current_player
-end
-
- def position_taken?(board, location)
-  board[location] != " " && board[location] != ""
-end
-
- def valid_move?(board, position)
-  position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
-end
-
- def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  if valid_move?(board, input)
-    move(board, input, current_player(board))
-  else
-    turn(board)
-  end
-  display_board(board)
-end
-
- def turn_count(board)
-  counter = 0
-  board.each do |i|
-    if i == "X" || i == "O"
-      counter += 1
-    end
-  end
-  return counter
-end
 
  def current_player(board)
   turn_count(board) % 2 == 0 ? "X" : "O"
@@ -76,45 +36,3 @@ end
   end
 end
 
- def full?(board)
-  if board.detect {|i| i == " " || i == nil}
-    return false
-  else
-    return true
-  end
-end
-
- def draw?(board)
-  if !won?(board) && full?(board)
-    return true
-  else
-    return false
-  end
-end
-
- def over?(board)
-  if draw?(board) || won?(board)
-    return true
-  else
-    return false
-  end
-end
-
- def winner(board)
-  if win_combination = won?(board)
-    board[win_combination.first]
-  end
-end
-
- def play(board)
-  until over?(board)
-    current_player(board)
-    turn(board)
-  end
-  if won?(board)
-    puts "Congratulations #{winner(board)}!"
-  else
-    puts "Cats Game!"
-  end
-end
-# Define your WIN_COMBINATIONS constant
